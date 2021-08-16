aeaa1a3f28fde6c9ff2d4b24b5c2d5b76076b097
Victor Torrao <victorct2@hotmail.com>
2021-08-16 16:30:41 -0300

    salve_alteração

--- application/views/index.php
+++ application/views/index.php
@@ -6,7 +6,7 @@
   <div class="container-fluid">
     <div class="hero">
       <hgroup>    
-    <h1>Avisos</h1><small><?php echo date("d/m/Y");?></small>
+    <h1>Avisos</h1><big><?php echo date("d/m/Y");?></big>
   <br>
       </hgroup>
     </div>
@@ -20,7 +20,7 @@
 
                    <div class="col-sm-6">        
                       <h3><a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>"></a></h3>
-<h3><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span><?php echo converteDataInterface($avisos->dia) .' :: '. $avisos->descricao ?> </h3>
+<h3><span class="glyphicon glyphicon-arrow-right " aria-hidden="true"></span><?php echo converteDataInterface($avisos->dia) .' :: '. $avisos->descricao ?> </h3>
 <p><a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>"><?php echo $avisos->sinopse ?>»»</a></p>
                   </div>
 
