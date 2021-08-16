6ddf1eb633f13f74bdb1276da429323e720645e3
Victor Torrao <victorct2@hotmail.com>
2021-08-16 11:57:04 -0300

    melhoria visual

--- application/views/index.php
+++ application/views/index.php
@@ -1,38 +1,30 @@
 <div class="wrapper">
-
   <?php $this->load->view('include/header') ?>
   <?php $this->load->view('include/menuLateral') ?>
-  
-
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Avisos
-        <small><?php echo date("d/m/Y");?></small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Avisos</li>
-      </ol>
-    </section>
-
-    <br>
-
+  <section id="parallaxBar" class="noticias" data-speed="6">
+  <div class="container-fluid">
+    <div class="hero">
+      <hgroup>    
+    <h1>Avisos</h1><small><?php echo date("d/m/Y");?></small>
+  <br>
+      </hgroup>
+    </div>
+    <div class="overlay-internos"></div>
+  </div>
+</section>
+<section>
+  <div class="container textos-internos "> 
         <?php foreach ($listAvisos as $key => $avisos) { ?>
           <?php  echo (!(intval($key) % 2) || $key == '0') ? '<div class="row linhaNoticia"> ' : '';   ?>   
 
                    <div class="col-sm-6">        
-                      <h3><a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>"></a>
-                          <?php echo converteDataInterface($avisos->dia) .' :: '. $avisos->descricao ?> 
-                      </h3>
-                      <p>
-                        <a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>">
-                          <?php echo $avisos->sinopse ?>»»
-                        </a>
-                      </p>
+                      <h3><a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>"></a></h3>
+<h3><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span><?php echo converteDataInterface($avisos->dia) .' :: '. $avisos->descricao ?> </h3>
+<p><a href="<?php echo base_url('Home/avisoAberto/'.$avisos->friendly_url)?>"><?php echo $avisos->sinopse ?>»»</a></p>
                   </div>
+
+
                 
             
             <?php echo ((intval($key) % 2) && $key != '0') ? '</div><hr> ' : '';?>                   
@@ -40,25 +32,19 @@
         <?php } ?>
         
 
-      <div class="row" >                       
+      </div>
+
+      
+</section>
+
+<div class="row" >                       
         <div  style="text-align:center"> 
             <?php echo $paginacao ?>
         </div>
-      </div>
+</div>
 
-  </div>
-                   
-
-        </section>
-        <!-- right col -->
-      </div>
-      <!-- /.row (main row) -->
+</div>
 
 
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
+    
   
